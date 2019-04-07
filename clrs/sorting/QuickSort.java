8ab8e6f7238a200ef43fe0e198fb0465b5d9e81f
Saylee Pradhan <sayleepradhan@yahoo.co.in>
2019-04-06 23:02:33 -0500

    Adds randomized quick-sort algorithm

--- clrs/sorting/QuickSort.java
+++ clrs/sorting/QuickSort.java
@@ -1,6 +1,23 @@
 package clrs.sorting;
 
+import java.util.Random;
+
 public class QuickSort {
+    private int randomizedPartition(int[] arr, int start, int end) {
+        Random random = new Random();
+        int randomIndex = random.nextInt((end-start) + 1)+start;
+        int temp = arr[end];
+        arr[end] = arr[randomIndex];
+        arr[randomIndex] = temp;
+        return getPartition(arr,start,end);
+    }
+    private void randomizedSort(int[] arr, int start, int end) {
+        if (start < end) {
+            int partition = randomizedPartition(arr, start, end);
+            randomizedSort(arr, start, partition-1);
+            randomizedSort(arr, partition+1, end);
+        }
+    }
     private void sort(int[] arr, int start, int end) {
         if (start < end) {
             int mid = getPartition(arr, start, end);
@@ -34,11 +51,22 @@ public class QuickSort {
     public static void main(String[] args) {
         QuickSort quickSort = new QuickSort();
         int[] arr = {2,8,7,1,3,5,6,4};
+        int[] arr2 = {2,8,7,1,3,5,6,4};
         quickSort.sort(arr,0,arr.length-1);
-
+        quickSort.randomizedSort(arr2,0,arr2.length-1);
+        System.out.println("Quick sort results:");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
             if (i != arr.length-1) {
+                System.out.print(" , ");
+            }
+        }
+
+        System.out.println("\n");
+        System.out.println("Randomized Quick sort results:");
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i]);
+            if (i != arr2.length-1) {
                 System.out.print(" , ");
             }
         }
